4807f03f1c37a659c8c83d5f100c3dcb361ae46c
guswns1944 <55592628+guswns1944@users.noreply.github.com>
2020-03-18 20:30:30 +0900

    Add files via upload
    first web page

---
+++ first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+ <head>
+  <title>나의 첫번째 php</title>
+ </head>
+
+ <body>
+ 여러분 우리 쇼핑몰 오신 것을 환영합니다.
+ <br>
+ 현재 접속하신 일자는
+ <?php
+ echo date("yy-m-d");
+ ?>
+ </body>
+</html>
